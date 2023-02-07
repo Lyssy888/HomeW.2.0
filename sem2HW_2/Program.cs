4c030aa4d0e0c47a753a25daa7f9deb78b43c6b9
Ruslan <888lyssy@gmail.com>
2023-02-07 23:58:53 +0500

    Задача 15

---
+++ sem2HW_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Write($"enter the day of the week: "); 
+int num = Convert.ToInt32(Console.ReadLine()); 
+if (num >= 1 && num <= 5) 
+{ 
+    Console.Write($"It's a working day");  
+} 
+ 
+else if (num >= 6 && num <= 7) 
+{ 
+    Console.Write($"it's a day off"); 
+} 
+else Console.Write($"there is no such day of the week");
